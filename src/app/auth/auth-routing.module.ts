5728131a634cd2953e710483ade00b09ca19cf8d
Tony Wang <Tony94042@gmail.com>
2023-10-21 11:52:48 +1100

    Added Fromcontrol and Formgroup

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent },
