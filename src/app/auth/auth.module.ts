b8c4e7e6616c9a1a21966e7d5b5c3ea384f4ade6
Tony Wang <Tony94042@gmail.com>
2023-10-21 13:05:13 +1100

    Added Basic Sync Validations

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatButtonModule } from '@angular/material/button'
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -19,8 +17,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatButtonModule
   ]
 })
