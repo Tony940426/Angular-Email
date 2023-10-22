dd61ec3a19d65a9ad9b0fad13819a96f50a97b65
Tony Wang <Tony94042@gmail.com>
2023-10-22 13:15:27 +1100

    Refractored HTTP call to service

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,27 +1,29 @@
 import { AsyncValidator, FormControl } from "@angular/forms";
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { map, catchError} from 'rxjs/operators'
+import { map, catchError} from 'rxjs/operators';
 import { of } from 'rxjs'
+import { AuthService } from "../auth.service";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class UniqueUsername implements AsyncValidator {
+    constructor(private authService: AuthService){}
+
     validate = (control: FormControl) => {
-        const {value} = control
-        return this.http.post<any>('https://api.angular-email.com/auth/username', {
-            username: value
-        }).pipe(
-            map((value) => {
-                //if the response contains a error, it skips pipes wuch as .map
+        const { value } = control
+        return this.authService.userNameAvailable(value)
+        .pipe(
+            map(value => {
+                //if the response contains a error, it skips pipes such as .map
                 if(value.available) {
                     return null;
                 }
             }),
             catchError((err) => {
-                if(err.error.message.username){
+                console.log(err)
+                if(err.message.username){
                     return of({ nonUniqueUsername: true});
                 } else {
                     return of ({noNetworkConnection: true});
@@ -29,5 +31,4 @@ export class UniqueUsername implements AsyncValidator {
             })
         )
     }
-    constructor(private http: HttpClient){}
 }
