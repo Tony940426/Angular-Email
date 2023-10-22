8121bdba248d7c8cb9b9227a0b187179dfaeb85e
Tony Wang <Tony94042@gmail.com>
2023-10-22 11:17:01 +1100

    Added first customer validator -Match!

--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,8 +1,16 @@
 import { Validator, FormGroup } from '@angular/forms'
 
 export class MatchPassword implements Validator{
+    //The implements Validator is optional. It just a helper. It tells you what is missing/gives
+    //you a hint/helping hand. 
     validate(formGroup: FormGroup){
-        return { };
-        
+    //If you don't know if you are validating form control or form group, you can fall back on control: AbstractControl
+    const { password, passwordConfirmation } = formGroup.value
+    
+        if(password === passwordConfirmation) {
+            return null;
+        } else {
+            return { PasswordDontMatch: true };
+        }
     }
 }
