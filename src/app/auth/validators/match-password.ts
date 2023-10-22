a308d501f6c52cf693c4b7e0531d16b83e9738d4
Tony Wang <Tony94042@gmail.com>
2023-10-22 11:39:37 +1100

    Turned off strict more. Implemented first custom validator to component

--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,16 +1,22 @@
+import { Injectable } from '@angular/core';
 import { Validator, FormGroup } from '@angular/forms'
+
+@Injectable(
+    {providedIn: 'root'}
+)
 
 export class MatchPassword implements Validator{
     //The implements Validator is optional. It just a helper. It tells you what is missing/gives
     //you a hint/helping hand. 
-    validate(formGroup: FormGroup){
     //If you don't know if you are validating form control or form group, you can fall back on control: AbstractControl
-    const { password, passwordConfirmation } = formGroup.value
-    
-        if(password === passwordConfirmation) {
+    validate(formGroup: FormGroup){
+    const password = formGroup.get('password').value;
+    const passwordConfirmation = formGroup.get('passwordConfirmation').value;
+
+        if(password == passwordConfirmation) {
             return null;
         } else {
             return { PasswordDontMatch: true };
         }
-    }
+    };
 }
